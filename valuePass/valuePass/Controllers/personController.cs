e32e9ea0092e9a7f8d42b6066e281c59fe616ef0
Shagorrakshit <shagorrakshit99@gmail.com>
2022-07-01 22:26:27 +0600

    first commit

---
+++ valuePass/valuePass/Controllers/personController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using valuePass.Models;
+
+namespace valuePass.Controllers
+{
+    public class personController : Controller
+    {
+      
+        
+        // GET: person
+        public ActionResult Information()
+        {
+            Person a1 = new Person()
+            {
+                Name = "Shagor Kanti Rakshit",
+                Age = "21",
+                Semester = "10th"
+            };
+            return View(a1); ;
+        }
+        
+    }
+}
---
+++ valuePass/valuePass/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace valuePass.Models
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Semester { get; set; }
+    }
+}
